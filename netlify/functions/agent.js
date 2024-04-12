c8f9153df58054afe6661d8703bd3914b43a7493
Kamil Kobylarz <xraxus@gmail.com>
2024-04-12 09:44:31 +0200

    Provide getCurrentWeather function to agent instead of a netlify function endpoint

--- netlify/functions/agent.js
+++ netlify/functions/agent.js
@@ -32,7 +32,6 @@ Here's an example response format:
     "weather": "Expect mild weather with a low of 19°C and a high of 25°C", 
     "flight": "The best option for you is with Delta Airlines with a layover in Oslo", "hotel": "We recommend you stay at the Premiere Inn hotel in central Paris"
 }
-RETURN JSON ONLY
 `,
     },
     { role: "user", content: JSON.stringify(tripData) },
@@ -53,13 +52,43 @@ RETURN JSON ONLY
 }
 
 // AGENT TOOLS
-export async function getCurrentWeather({ location }) {
+async function getCurrentWeather({ location }) {
   try {
-    const response = await fetch(
-      `/.netlify/functions/getCurrentWeather?location=${location}`
+    const { lat, lon } = await getCoordinates(location);
+    const weatherUrl = new URL(
+      "https://api.openweathermap.org/data/2.5/weather"
     );
-    const data = await response.json();
+
+    weatherUrl.searchParams.append("lat", lat);
+    weatherUrl.searchParams.append("lon", lon);
+    weatherUrl.searchParams.append("units", "metric");
+    weatherUrl.searchParams.append(
+      "appid",
+      import.meta.env.VITE_OPENWEATHER_API_KEY
+    );
+    const res = await fetch(weatherUrl);
+    const data = await res.json();
     return JSON.stringify(data);
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function getCoordinates(city) {
+  console.log(city);
+  try {
+    const weatherUrl = new URL("http://api.openweathermap.org/geo/1.0/direct");
+    weatherUrl.searchParams.append("q", city);
+    weatherUrl.searchParams.append("limit", 1);
+    weatherUrl.searchParams.append(
+      "appid",
+      import.meta.env.VITE_OPENWEATHER_API_KEY
+    );
+    const res = await fetch(weatherUrl);
+    const data = await res.json();
+    const { lat, lon } = data[0];
+    console.log(data, lat, lon);
+    return { lat, lon };
   } catch (err) {
     console.error(err.message);
   }
